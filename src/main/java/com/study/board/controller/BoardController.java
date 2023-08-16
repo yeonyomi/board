d688817d19d664ba1ce2e8cbba31bc2e8d7b05c4
yeonhwa <dusdyal25@gmail.com>
2023-08-17 04:37:22 +0900

    Initial commit

---
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -0,0 +1,43 @@
+package com.study.board.controller;
+
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BoardController {
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board/write")//localhost:8080/board/write
+    public String boardWriteForm() {
+
+        return "boardwrite";
+    }
+
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board) {
+
+        boardService.write(board);
+        return "";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model) {
+
+        model.addAttribute("list",boardService.boardList());
+
+        return"boardlist";
+}
+
+        @GetMapping("/board/view")
+    public String boardview(){
+        return "boardview";
+        }
+}
